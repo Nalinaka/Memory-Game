f2e7a9007e877fcb20cb040d854139b6543e3fb4
Nalinaka <nalinaka@gmail.com>
2023-05-01 23:26:10 -0700

    Committing changes to HTML and start of CSS

---
+++ test.js
@@ -0,0 +1,26 @@
+// Set up HTML for overall gaming board - completed 
+// set up HTML and also set up the 
+
+// Link to CSS and JS - completed
+
+// Set up folders:
+// app.js, test.js, syle.css, read.me all set up 
+
+// Set up css folder:
+
+// button settings - completed
+// set up back ground image for game - completed
+
+// Upload good and bad character images from star wars
+
+// Assign the images to the card game 
+
+// Set up JS folder:
+
+// Finish up any additional items
+
+// write if statements for outcomes of good or bad 
+
+// test game and all code and use console.log
+
+// Ask 5 random people to test game! 
