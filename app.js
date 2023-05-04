b06a448911c6814c369198b66b5ab8cf166ce43b
Nalinaka <nalinaka@gmail.com>
2023-05-04 13:12:48 -0700

    Added board game cards and debugging card issue

--- app.js
+++ app.js
@@ -1,158 +1,199 @@
-document.addEventListener("DOMContentLoaded", function () {
+// Clicking on Enter button
+// New images displayed of hidden cards = creating board
+// then game startes - shuffling board
     //define functions 
-    
-    createBoard(grid, cardArray); 
-    arrangeCard();
-    playAgain.addEventListener("click", replay); 
-    
-    //add a click function for images 
-    
-    imgs = document.querySelectorAll("img");
-    Array.from(imgs).forEach(img => 
-    img.addEventListener("click", flipCard)
-    ) 
-    });
-    
-const buttonsContainer = document.querySelector(".buttons-container");
-let cards = []
-let firstCard, secondCard; 
-let lockBoard = false; 
-let score = 0;
+    // fit screen in css or late
 
-document.querySelector(".score").textContent = score; 
 
-fetch("./data/cards.json")
-.then((res) => res.json())
-.then((data) => {
-    cards = [...data,...data];
-    shuffleCards();
-    generateCards();
-});
+    let cardArray = [
+        {name: "Yoda", id:"Yoda1", img:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT8ApB54YlPe5lmJ41Zo415suCA9sWim_aTHA&usqp=CAU", },
+        {name: "Yoda", id:"Yoda2", img:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT8ApB54YlPe5lmJ41Zo415suCA9sWim_aTHA&usqp=CAU", },
+        {name: "Grogu", id:"Grogu1", img:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ2JPwGc930J9FkQ2fxykEUWlUkCwtotSbmHg&usqp=CAU", },
+        {name: "Grogu", id:"Grogu2", img:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ2JPwGc930J9FkQ2fxykEUWlUkCwtotSbmHg&usqp=CAU", },
+        {name: "Darth Vader", id:"DarthVader1", img:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSZuxLoZiu9cyoCq5fwPK3W5pIqhFlAQeEcyA&usqp=CAU", },
+        {name: "Darth Vader", id:"DarthVader2", img:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT1wcFyVnejaqJWWu1940Nte5ifeKMiPbrlbA&usqp=CAU", },
+        {name: "Darth Maul", id:"DarthMaul1", img:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT1wcFyVnejaqJWWu1940Nte5ifeKMiPbrlbA&usqp=CAU", },
+        {name: "Darth Maul", id:"DarthMaul2", img:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT1wcFyVnejaqJWWu1940Nte5ifeKMiPbrlbA&usqp=CAU", },
+        {name: "Jabba the Hutt", id:"JabbatheHutt1", img:"https://static.wikia.nocookie.net/disney/images/d/df/Profile_-_Jabba_the_Hutt.jpg/revision/latest?cb=20190728025948", },
+        {name: "Jabba the Hutt", id:"JabbatheHutt2", img:"https://static.wikia.nocookie.net/disney/images/d/df/Profile_-_Jabba_the_Hutt.jpg/revision/latest?cb=20190728025948", },
+        {name: "Hans Solo", id:"HansSolo1", img:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQYM7DA6gL2xgCP8lkybHNNImEMirwuX4zqyQ&usqp=CAU", },
+        {name: "Hans Solo", id:"HansSolo2", img:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQYM7DA6gL2xgCP8lkybHNNImEMirwuX4zqyQ&usqp=CAU", },
+        {name: "Kylo Ren", id:"KyloRen1", img:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR6b1PyzX_LhHkeMykPjW2kBUQFGukAcT_3ew&usqp=CAU", },
+        {name: "Kylo Ren", id:"KyloRen2", img:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR6b1PyzX_LhHkeMykPjW2kBUQFGukAcT_3ew&usqp=CAU", },
+        {name: "The Mandalorian", id:"TheMandalorian1", img:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSNKnm5e6G5H021X9UBvvaDR5-wWIbyAJ6m5A&usqp=CAU", },
+        {name: "The Mandalorian", id:"TheMandalorian2", img:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSNKnm5e6G5H021X9UBvvaDR5-wWIbyAJ6m5A&usqp=CAU", },
+        {name: "Barriss Offee", id:"BarrissOffee1", img:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR_6AfhevbacAZ9exIjIuZvyseziI6NE6T9eg&usqp=CAU", },
+        {name: "Barriss Offee", id:"BarrissOffee2", img:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR_6AfhevbacAZ9exIjIuZvyseziI6NE6T9eg&usqp=CAU", },
+        {name: "Ashoka Tano", id:"AshokaTano1", img:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRtmkuqrN4Dz80Qs5bbL4M5999OUNtXLIQRMA&usqp=CAU", },
+        {name: "Ashoka Tano", id:"AshokaTano2", img:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRtmkuqrN4Dz80Qs5bbL4M5999OUNtXLIQRMA&usqp=CAU", },
+       
+    ]
 
-function shuffleCards() {
-    let currentIndex = cards.length,
-    randomIndex,
-    temporaryValue;
-    while(currentIndex !==0) {
-        randomIndex = Math.floor(Math.random() * currentIndex);
-        currentIndex -= 1;
-        temporaryValue = cards[currentIndex];
-        cards[currentIndex] = cards[randomIndex];
-        cards[randomIndex] = temporaryValue;
+    const boardContainer = document.getElementById("Board-Container")
+
+
+    // const game = {
+    //     backImages: [cardArray],
+    //     cardsSelected: [],
+    //     cardsMatched: [],
+    //     pendingMoves: 100, 
+    //     shuffledBackImages: [],
+    //     time: 0,
+    //     timeInterval: 0
+    // }
+    
+    //event listener for play button
+const playButton = document.querySelector("#play")
+playButton.addEventListener('click', createBoard)
+
+//event listener for quit button
+//const quitButton = document.querySelector("#quit")
+
+// // discuss below code 
+// playButton.style.cursor = 'default'
+// playButton.removeEventListener('click', addCards)
+// quitButton.addEventListener('click', quitGame)
+// gameStart()
+
+
+function createBoard () {
+    // const addCards = document.querySelector("addcards")
+    for (let x=0; x < cardArray.length; x++) {
+        let card = document.createElement('div')
+        boardContainer.appendChild(card)
+    
     }
+
+     //   card.addEventListener('click, flipCard')
 }
 
-function generateCards() {
-    for (let cards of cards) {
-        const cardElement = document.createElement("div");
-        cardElement.classList.add("card");
-        cardElement.setAttribute("data-name", card.name);
-        cardElement.innerHTML = ` 
-    <div class="front">
-    <img class="front-image" src=${card.image} />
-    </div>
-        <div class="back"></div>
-    `;
-gridContainer.appendChild(cardElement);
-cardElement.addEventListener("click", flipcard);
-}
-}
+// function flipCard () {
+//     if(lockBoard) return;
+//     if (this === firstCard) return;
 
-function flipCard () {
-    if(lockBoard) return;
-    if (this === firstCard) return;
+//     this.classList.add("flipped");
 
-    this.classList.add("flipped");
+//     if (!firstCard) {
+//         firstCard = this;
+//         return; 
 
-    if (!firstCard) {
-        firstCard = this;
-        return; 
-
-    secondCard = this;
-    score ++;
-    document.querySelector(".score").textContent = score;
-    lockBoard = true;
-    }
-function matchCheck ();
+//     if (secondCard) = this;
+//     score ++;
+//     document.querySelector(".score").textContent = score;
+//     lockBoard = true;
+//     }
+// function matchCheck ();
   
-}
+// }
 
-function matchCheck () {
-    let isMatch = firstCard.datasetname === secondCard.dataset.name;
+// function matchCheck () {
+//     let isMatch = firstCard.datasetname === secondCard.dataset.name;
 
-    isMatch ? disableCards () : unflipCards();
-}
+//     isMatch ? disableCards () : unflipCards();
+// }
 
-function disableCards() {
-    firstCard.removeEventListener("click", flipCard);
-    secondCard.removeEventListener("click", flipCard);
+// function disableCards() {
+//     firstCard.removeEventListener("click", flipCard);
+//     secondCard.removeEventListener("click", flipCard);
 
-    resetBoard();
-}
+//     resetBoard();
+// }
 
-function unflipCards() {
-    setTimeout(() => {
-        firstCard.classList.remove("flipped");
-        secondCard.classList.remove("flipped");
-        resetBoard();
-    }, 1000);
-}
+// function unflipCards() {
+//     setTimeout(() => {
+//         firstCard.classList.remove("flipped");
+//         secondCard.classList.remove("flipped");
+//         resetBoard();
+//     }, 1000);
+// }
 
-function resetBoard() {
-    firstCard = null;
-    secondCard = null;
-    lockBoard = false; 
-}
+// function resetBoard() {
+//     firstCard = null;
+//     secondCard = null;
+//     lockBoard = false; 
+// }
 
-function restart() {
-    resetBoard();
-    shuffleCards();
-    score = 0;
-    document.querySelector(".score").textContent = score;
-    gridContainer.innerHTML = "";
-    generateCards();
-}
-// following code above from https://youtu.be/xWdkt6KSirw
+// function restart() {
+//     resetBoard();
+//     shuffleCards();
+//     score = 0;
+//     document.querySelector(".score").textContent = score;
+//     gridContainer.innerHTML = "";
+//     generateCards();
+// }
+// // following code above from https://youtu.be/xWdkt6KSirw
 
 
-function checkWon() {
-if (cardsWon == cardArray.length / 2) {
-alert("You won") 
-setTimeout(()=> popup.style.display = "flex" ,300); 
-}
-}
+// function checkWon() {
+// if (cardsWon == cardArray.length / 2) {
+// alert("You won") 
+// setTimeout(()=> popup.style.display = "flex" ,300); 
+// }
+// }
 
-let cardArray = [
-    {name: "Yoda", img:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT8ApB54YlPe5lmJ41Zo415suCA9sWim_aTHA&usqp=CAU", },
-    {name: "Yoda", img:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT8ApB54YlPe5lmJ41Zo415suCA9sWim_aTHA&usqp=CAU", },
-    {name: "Grogu", img:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ2JPwGc930J9FkQ2fxykEUWlUkCwtotSbmHg&usqp=CAU", },
-    {name: "Grogu", img:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ2JPwGc930J9FkQ2fxykEUWlUkCwtotSbmHg&usqp=CAU", },
-    {name: "Darth Vader", img:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSZuxLoZiu9cyoCq5fwPK3W5pIqhFlAQeEcyA&usqp=CAU", },
-    {name: "Darth Vader", img:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT1wcFyVnejaqJWWu1940Nte5ifeKMiPbrlbA&usqp=CAU", },
-    {name: "Darth Maul", img:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT1wcFyVnejaqJWWu1940Nte5ifeKMiPbrlbA&usqp=CAU", },
-    {name: "Darth Maul", img:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT1wcFyVnejaqJWWu1940Nte5ifeKMiPbrlbA&usqp=CAU", },
-    {name: "Jabba the Hutt", img:"https://static.wikia.nocookie.net/disney/images/d/df/Profile_-_Jabba_the_Hutt.jpg/revision/latest?cb=20190728025948", },
-    {name: "Jabba the Hutt", img:"https://static.wikia.nocookie.net/disney/images/d/df/Profile_-_Jabba_the_Hutt.jpg/revision/latest?cb=20190728025948", },
-    {name: "Hans Solo", img:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQYM7DA6gL2xgCP8lkybHNNImEMirwuX4zqyQ&usqp=CAU", },
-    {name: "Hans Solo", img:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQYM7DA6gL2xgCP8lkybHNNImEMirwuX4zqyQ&usqp=CAU", },
-    {name: "Kylo Ren", img:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR6b1PyzX_LhHkeMykPjW2kBUQFGukAcT_3ew&usqp=CAU", },
-    {name: "Kylo Ren", img:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR6b1PyzX_LhHkeMykPjW2kBUQFGukAcT_3ew&usqp=CAU", },
-    {name: "The Mandalorian", img:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSNKnm5e6G5H021X9UBvvaDR5-wWIbyAJ6m5A&usqp=CAU", },
-    {name: "The Mandalorian", img:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSNKnm5e6G5H021X9UBvvaDR5-wWIbyAJ6m5A&usqp=CAU", },
-    {name: "Barriss Offee", img:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR_6AfhevbacAZ9exIjIuZvyseziI6NE6T9eg&usqp=CAU", },
-    {name: "Barriss Offee", img:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR_6AfhevbacAZ9exIjIuZvyseziI6NE6T9eg&usqp=CAU", },
-    {name: "Ashoka Tano", img:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRtmkuqrN4Dz80Qs5bbL4M5999OUNtXLIQRMA&usqp=CAU", },
-    {name: "Ashoka Tano", img:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRtmkuqrN4Dz80Qs5bbL4M5999OUNtXLIQRMA&usqp=CAU", },
-https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSNKnm5e6G5H021X9UBvvaDR5-wWIbyAJ6m5A&usqp=CAU
-]
 
-let grid = document.querySelector(".grid"); 
-let scoreBoard = document.querySelector(".scoreBoard"); 
-let popup = document.querySelector(".popup"); 
-let playAgain = document.querySelector(".playAgain"); 
-let clickBoard = document.querySelector(".clickBoard"); 
-let imgs; 
-let cardsId = []; 
-let cardsSelected = []; 
-let cardsWon = 0; 
-let clicks = 0;
+// // https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSNKnm5e6G5H021X9UBvvaDR5-wWIbyAJ6m5A&usqp=CAU
+
+
+// let grid = document.querySelector(".grid"); 
+// let scoreBoard = document.querySelector(".scoreBoard"); 
+// let popup = document.querySelector(".popup"); 
+// let playAgain = document.querySelector(".playAgain"); 
+// let clickBoard = document.querySelector(".clickBoard"); 
+// let imgs; 
+// let cardsId = []; 
+// let cardsSelected = []; 
+// let cardsWon = 0; 
+// let clicks = 0;
+//     //add a click function for images 
+    
+//     imgs = document.querySelectorAll("img");
+//     Array.from(imgs).forEach(img => 
+//     img.addEventListener("click", flipCard)
+//     ) 
+    
+//     document.addEventListener("DOMContentLoaded", function () {
+//     })
+
+    
+// const buttonsContainer = document.querySelector(".buttons-container");
+// let cards = []
+// let firstCard, secondCard; 
+// let lockBoard = false; 
+// let score = 0;
+
+// document.querySelector(".score").textContent = score; 
+
+// fetch("./data/cards.json")
+// .then((res) => res.json())
+// .then((data) => {
+//     cards = [...data,...data];
+//     shuffleCards();
+//     generateCards();
+// });
+
+   // player.innerHTML = ''
+
+    // function getBackImage(i) {
+    //     let imageArray = game.shuffledBackImages
+    //     return imageArray[i]
+    //  }}
+     
+    // function shuffleCards(cardArray) {
+    //     let currentIndex = cards.length,
+    //     randomIndex,
+    //     temporaryValue;
+    //     while(currentIndex !==0) {
+    //         randomIndex = Math.floor(Math.random() * currentIndex);
+    //         currentIndex -= 1;
+    //     }
+    //            }
+    // }
+
+
+       //     temporaryValue = cards[currentIndex];
+         //   cards[currentIndex] = cards[randomIndex];
+          //  cards[randomIndex] = temporaryValue;
+ 
+        //remove event listener from play button once cards are added to the card holder
+     
